e9daa6c550045e9f9f7276b21609fd329fe57861
francesnicole69 <alsonchinn1@gmail.com>
2025-01-26 23:59:58 +0800

    Update Terminal.cs

--- s10266700G_Prg/Terminal.cs
+++ s10266700G_Prg/Terminal.cs
@@ -1,65 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using s10266700G_Prg;
 
-namespace s10266700G_Prg
+internal class Terminal
 {
-    internal class Terminal
+    private string terminalName;
+    public string TerminalName
     {
-        private string terminalName;
-        public string TerminalName
+        get { return terminalName; }
+        set { terminalName = value; }
+    }
+    private Dictionary<string, Airline> airlines;
+    public Dictionary<string, Airline> Airlines
+    {
+        get { return airlines;}
+        set { airlines = value; }
+    }
+    private Dictionary<string, Flight> flights;
+    public Dictionary<string, Flight> Flights
+    {
+     get{ return flights;}
+     set { flights = value; }
+    }
+    private Dictionary<string, BoardingGate> boardingGates;
+    public Dictionary<string, BoardingGate> BoardingGates
+    {
+        get { return boardingGates;}
+        set { boardingGates = value; }
+    }
+    private Dictionary<string, double> gateFees;
+    public Dictionary<string, double> GateFees
+    {
+        get { return gateFees;}
+        set { gateFees = value; }
+    }
+    public void PrintAirlineFees()
+    {
+        foreach (var (i, v) in airlines)
         {
-            get { return terminalName; }
-            set { terminalName = value; }
-        }
-        private Dictionary<String, Airline> airlines;
-        public Dictionary<String, Airline> Airlines
-        {
-            get { return airlines; }
-            set { airlines = value; }
-        }
-        private Dictionary<String, Flight> flights;
-        public Dictionary<String, Flight> Flights
-        {
-            get { return flights; }
-            set { flights = value; }
-        }
-        private Dictionary<String, BoardingGate> boardingGates;
-        public Dictionary<String, BoardingGate> BoardingGates
-        {
-            get { return boardingGates; }
-            set { boardingGates = value; }
-        }
-        private Dictionary<String, double> gateFees;
-        public Dictionary<String, double> GateFees
-        {
-            get { return gateFees; }
-            set { gateFees = value; }
-        }
-        public Terminal(string terminalName, Dictionary<String, Airline> airlines, Dictionary<String, Flight> flights, Dictionary<String, BoardingGate> boardingGates,Dictionary<String, double> gatefees)
-        {
-            TerminalName = terminalName;
-            Airlines = airlines;
-            Flights = flights;
-            BoardingGates = boardingGates;
-            GateFees = gateFees;
-        }
-        public bool AddAirline(Airline)
-        {
-            
-        }
-        public bool AddBoardingGate(BoardingGate)
-        {
-
-        }
-        
-
+            Console.WriteLine(Convert.ToString($"Flight {v.Name} fees are ${v.CalculateFees()}"));
         }
     }
-
-    
-   
-
-
+    public bool AddAirline(Airline add)
+    {
+        airlines.Add(add.Name, add);
+        return true;
+    }
+    public bool AddBoardingGate(BoardingGate add)
+    {
+        boardingGates.Add(add.GateName, add);
+        return true;
+    }
+    public Airline GetAirlineFromFlight(Flight selected)
+    {
+        foreach (var (i, v) in airlines)
+        {
+            var dictionary = v.Flights;
+            foreach (var (x, k) in dictionary)
+            {
+                if (k == selected)
+                {
+                    return v;
+                }
+            }
+        }
+        return null;
+    }
+    public override string ToString()
+    {
+        return $"This is terminal {terminalName}";
+    }
+} 
